a283560da2eb4fdaf95917b4dab80248963c0497
PiotrFLEURY <piotr.fleury@gmail.com>
2026-05-31 16:23:46 +0200

    fix(web): fix translations cached on web

--- potato_app/lib/main.dart
+++ potato_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:potato/app.dart';
+import 'package:potato/generated/codegen_loader.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,7 @@ void main() async {
       supportedLocales: const [Locale('en'), Locale('fr')],
       path: 'assets/translations',
       fallbackLocale: const Locale('en'),
+      assetLoader: const CodegenLoader(),
       child: ProviderScope(child: const PotatoApp()),
     ),
   );
